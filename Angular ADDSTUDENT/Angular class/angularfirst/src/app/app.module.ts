e8382b76e5377e6ddeba9c8eef640016104eb308
unknown <phyinpholuwa97@gmail.com>
2022-07-09 11:39:46 -0700

    My Hostel Project

---
+++ Angular ADDSTUDENT/Angular class/angularfirst/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+import { InterceptorService } from './services/interceptor.service';
+
+import { SignupComponent } from './signup/signup.component';
+import { UserendComponent } from './userend/userend.component';
+import { AdminendComponent } from './adminend/adminend.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    UserendComponent,
+    AdminendComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass:InterceptorService, multi:true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
